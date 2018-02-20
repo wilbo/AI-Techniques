04b2ee190e0f170b2ac78e0a5f2afea21452afda
Wilbert Schepenaar <wilbertschepenaar@hotmail.com>
2018-02-20 20:15:33 +0100

    more functionality to Vector2D class

---
+++ src/utils/IVector2D.ts
@@ -0,0 +1,94 @@
+import Vector2D from './Vector2D'
+
+interface IVector2D {
+	x: number
+	y: number
+
+	/**
+		* Check wether both x and y are zero
+		*/
+	isZero(): boolean
+
+	/**
+		* Set x and y both to zero
+		*/
+	zero(): void
+
+	/**
+		* The length / magnitude of the vector
+		*/
+	length(): number
+
+	/**
+		* The squared length of the vector (thereby avoiding the sqrt)
+		*/
+	lengthSq(): number
+
+	/**
+		* Normalizes the vector
+		*/
+	normalize(): Vector2D
+
+	/**
+		* The dot product of this and v2
+		*/
+	dot(vector: Vector2D): number
+
+	/**
+		* Returns positive if v2 is clockwise of this vector, negative if counterclockwise
+		* (assuming the Y axis is pointing down, X axis to right like a Window app)
+		*/
+	sign(vector: Vector2D): number
+
+	/**
+		* The vector that is perpendicular to this one
+		*/
+	perp(): Vector2D
+
+	/**
+		* Adjusts x and y so that the length of the vector does not exceed max
+		*/
+	truncate(max: number): Vector2D
+
+	/**
+		* The distance between this and the vector
+		*/
+	distance(vector: Vector2D): number
+
+	/**
+		* The distance between this and the vector squared
+		*/
+	distanceSq(vector: Vector2D): number
+
+	/**
+		* returns the vector that is the reverse of this vector
+		*/
+	reverse(): Vector2D
+
+	/**
+		* Adds a vector to the current vector
+		*/
+	add(vector: Vector2D): Vector2D
+
+	/**
+		* Subtracts a vector to the current vector
+		*/
+	subtract(vector: Vector2D): Vector2D
+
+	/**
+		* Multiplies a vector to the current vector
+		*/
+	multiply(value: number): Vector2D
+
+	/**
+		* Divides a vector to the current vector
+		*/
+	divide(value: number): Vector2D
+
+	/**
+		* Check's wether the vector is equal to the current vector
+		*/
+	equals(vector: Vector2D): boolean
+}
+
+export default IVector2D
--- src/utils/Vector2D.ts
+++ src/utils/Vector2D.ts
@@ -1,8 +1,94 @@
-class Vector2D {
+import IVector2D from './IVector2D'
+
+class Vector2D implements IVector2D {
 	constructor(
 		public x: number = 0,
 		public y: number = 0,
 	) { }
+
+	public zero(): void {
+		this.x = 0
+		this.y = 0
+	}
+
+	public isZero(): boolean {
+		return this.x === 0 && this.y === 0
+	}
+
+	public length(): number {
+		return Math.sqrt(Math.pow(this.x, 2) + Math.pow(this.y, 2))
+	}
+
+	public normalize(): Vector2D {
+		const length = this.length()
+		this.x /= length
+		this.y /= length
+		return this
+	}
+
+	public lengthSq(): number {
+		throw new Error('Method not implemented.')
+	}
+
+	public dot(vector: Vector2D): number {
+		throw new Error('Method not implemented.')
+	}
+
+	public sign(vector: Vector2D): number {
+		throw new Error('Method not implemented.')
+	}
+
+	public perp(): Vector2D {
+		throw new Error('Method not implemented.')
+	}
+
+	public truncate(max: number): Vector2D {
+		throw new Error('Method not implemented.')
+	}
+
+	public distance(vector: Vector2D): number {
+		throw new Error('Method not implemented.')
+	}
+
+	public distanceSq(vector: Vector2D): number {
+		throw new Error('Method not implemented.')
+	}
+
+	public reverse(): Vector2D {
+		throw new Error('Method not implemented.')
+	}
+
+	/**
+		* Operators
+		*/
+
+	public add(vector: Vector2D): Vector2D {
+		this.x += vector.x
+		this.y += vector.y
+		return this
+	}
+
+	public subtract(vector: Vector2D): Vector2D {
+		this.x -= vector.x
+		this.y -= vector.y
+		return this
+	}
+
+	public multiply(value: number): Vector2D {
+		this.x *= value
+		this.y *= value
+		return this
+	}
+
+	public divide(value: number): Vector2D {
+		this.x *= value
+		this.y *= value
+		return this
+	}
+
+	public equals(vector: Vector2D): boolean {
+		return vector.x === this.x && vector.y === this.y
+	}
 }
 
 export default Vector2D
