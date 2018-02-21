af0d1a26f5061dfc747d712d87e8c610c12e7efe
Wilbert Schepenaar <wilbertschepenaar@hotmail.com>
2018-02-21 20:07:41 +0100

    Added fps disply

--- src/game/Frame.ts
+++ src/game/Frame.ts
@@ -9,11 +9,18 @@ class Frame {
 	private _inactiveTime = 0
 	private _stopTime = 0
 
+	private _fps = 60
+	private _framesThisSecond = 0
+	private _lastFpsUpdate = 0
+
 	constructor(private _world: World) { }
 
 	public start(): void {
-		this._running = true
+		// track inactive time to allow the game to be paused
 		this._inactiveTime += (performance.now() - this._stopTime)
+
+		// start the loop
+		this._running = true
 		requestAnimationFrame(this.loop)
 	}
 
@@ -37,28 +44,31 @@ class Frame {
 			return
 		}
 
+		// fps calculations
+		if (timestamp > this._lastFpsUpdate + 1000) { // update every second
+			this._fps = 0.25 * this._framesThisSecond + (1 - 0.25) * this._fps // 0.25 is the decay
+			this._world.fps = this._fps
+
+			this._lastFpsUpdate = timestamp
+			this._framesThisSecond = 0
+		}
+
+		this._framesThisSecond++
+
+		// track the accumulated time that hasn't been simulated yet
 		this._delta += timestamp - this._lastFrameTimeMs
 		this._lastFrameTimeMs = timestamp
 
-		let numUpdateSteps = 0
+		// simulate the total elapsed time in fixed-size chunks
 		while (this._delta >= this._timestep) {
-			this._world.update(this._timestep)
+			this._world.update(this._timestep / 1000)
 			this._delta -= this._timestep
-
-			if (++numUpdateSteps >= 240) {
-				this.panic()
-				break
-			}
 		}
 
+		// split the render from the update
 		this._world.render()
 		requestAnimationFrame(this.loop)
 	}
-
-	private panic(): void {
-		this._delta = 0
-	}
-
 }
 
 export default Frame
--- src/game/World.ts
+++ src/game/World.ts
@@ -1,7 +1,10 @@
 import * as SVG from 'svg.js'
 import EntityList from '../entity/EntityList'
+import Vector2D from '../utils/Vector2D'
 
 class World {
+	public fps: number = 0
+
 	constructor(
 		public height: number,
 		public width: number,
@@ -18,7 +21,22 @@ class World {
 		this._context.clear()
 		for (const entity of EntityList.instance.list) {
 			entity.render(this._context)
+			this.wrapAround(entity.position, this.width, this.height)
 		}
+
+		this.drawFps()
+	}
+
+	// Treats the world as a toroid
+	private wrapAround(position: Vector2D, maxX: number, maxY: number): void {
+		if (position.x > maxX) { position.x = 0.0 }
+		if (position.x < 0) { position.x = maxX }
+		if (position.y < 0) { position.y = maxY }
+		if (position.y > maxY) { position.y = 0.0 }
+	}
+
+	private drawFps(): void {
+		this._context.text(this.fps.toFixed(2) + ' fps').move(-(this.width / 2) + 10, this.height / 2 - 28)
 	}
 }
 
