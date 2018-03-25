86600a40fec5590f89b0374716484df90ab5e0ab
Wilbert Schepenaar <wilbertschepenaar@hotmail.com>
2018-03-26 01:00:59 +0200

    Added poi info to GetFuel state

--- src/state/states/vehicle/GetFuel.ts
+++ src/state/states/vehicle/GetFuel.ts
@@ -3,28 +3,38 @@ import Vehicle from '../../../entity/Vehicle'
 import Vector2D from '../../../utils/Vector2D'
 import FillTank from './FillTank'
 import DoNothing from './DoNothing'
+import PointOfInterest from '../../../configurations/levels/base/PointOfInterest'
 
 class GetFuel implements IState<Vehicle> {
 	public name = 'get fuel'
 
+	private _targetPoi: PointOfInterest
+
 	public enter(vehicle: Vehicle): void {
 		vehicle.steering.reset()
 
+		this._targetPoi = vehicle.world.level.poi('pit')
+		this._targetPoi.reserved = true
+
 		// initialize path following
-		const path = vehicle.world.findPath(vehicle.position, vehicle.world.level.poi('pit').point)
+		const path = vehicle.world.findPath(vehicle.position, this._targetPoi.point)
 		vehicle.steering.targetPositions = path
 		vehicle.steering.targetPosition = vehicle.steering.targetPositions[0]
 		vehicle.steering.followPathOn = true
 	}
 
 	public execute(vehicle: Vehicle): void {
-		if (Vector2D.equalsRounded(vehicle.position, vehicle.world.level.poi('pit').point, 24)) {
+		if (Vector2D.equalsRounded(vehicle.position, this._targetPoi.point, 24)) {
+			this._targetPoi.occupied = true
 			vehicle.stop()
 			vehicle.stateMachine.changeState(new FillTank())
 		}
 	}
 
-	public exit(vehicle: Vehicle): void {}
+	public exit(vehicle: Vehicle): void {
+		this._targetPoi.occupied = false
+		this._targetPoi.reserved = false
+	}
 }
 
 export default GetFuel
